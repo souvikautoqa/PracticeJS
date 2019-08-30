30ff90436149c9f472cb03c6fbef7505a8c49400
Dutta <duttas@dub-lp014434.pubedu.hegn.us>
2019-08-30 17:02:31 +0100

    still practicing

--- Variables.js
+++ Variables.js
@@ -1,7 +1,24 @@
-let number = 10;
-let name = 'Souvik';
-let temp;
+// Working with variables
 
-const score = 100;
-//score =200;
-console.log(name + number+temp);
+var name = 'John'; console.log(name)  // Basic variable
+
+// Data types  - Java Script has Dynamic Typing that means it figures out the data type by itself
+    // Primitive data types
+    var number = 1; // JS has all number as floating type, that means 
+                    // it has a decimal in the end by default
+    var names = 'Java Script';console.log(names); //  Sequence of characters
+    var status = true;console.log(status); //  Boolean with true and false
+    var job; console.log(job); // Undefined means that a variable that doesnt have a value 
+    var someObject = null; console.log(someObject); // Null means non existent
+
+    // Auto type casting
+    var fname = 'John';
+    var age = 28;
+
+    console.log(typeof fname);
+    console.log(typeof age);
+    console.log (fname + ' -- ' + age + ' is of type '+typeof (fname + age));
+
+    
+    // Non primitive data types (Object based)
+    var anotherObject = this; console.log(anotherObject);
---
+++ old lessons/Variables.js
@@ -0,0 +1,7 @@
+let number = 10;
+let name = 'Souvik';
+let temp;
+
+const score = 100;
+//score =200;
+console.log(name + number+temp);
